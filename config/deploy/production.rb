a67363fee3196eb616e621154ed50543fbc5df7d
kuriyamawataru <watarukuriyama02182@gmail.com>
2020-04-27 21:05:13 +0900

    commit1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '13.115.219.186', user: 'ec2-user', roles: %w{app db web}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  server '13.115.219.186/', user: 'ec2-user', roles: %w{app db web}
 end
